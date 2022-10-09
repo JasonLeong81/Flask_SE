58493811428c48d23541db3e4771487c66acc239
Jason <leongjason3781@gmail.com>
2022-10-09 23:34:41 +0800

    Showed how to retrieve data from html input and print on cmd

--- hello.py
+++ hello.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='template',static_folder='template/static')
 
 
 # @app.route('/')
 # @app.route('/index')
-@app.route('/testing')
+@app.route('/testing', methods=['GET', 'POST'])
 def index():
+	if request.method =='POST':
+		print(request.form.get('first_name'))
 	return render_template('index.html')
+
+
 
 # this is so the app won't run if the file is imported somewhere, run using a different methed, etc.
 if __name__ == '__main__':
